0ba09e35fb8f6b14751ff91532bf180cc583d044
andriyleshiy <andriyleshiy@gmail.com>
2017-02-22 19:39:24 +0200

    map ready

--- app/src/main/java/com/example/leshik/oskar/MainActivity.java
+++ app/src/main/java/com/example/leshik/oskar/MainActivity.java
@@ -1,32 +1,44 @@
 package com.example.leshik.oskar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-
-
+    SupportMapFragment fragment;
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((MapFragment) getFragmentManager().findFragmentById(
-                R.id.map)).getMapAsync(this);
+        fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
 
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        LatLng oskar = new LatLng(49.449917, 32.047750);
+        map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.setBuildingsEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
 
+        map.addMarker(new MarkerOptions().position(oskar).title("Oskar bar"));
+
+        CameraPosition pos = CameraPosition.builder().target(oskar).zoom(15).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(pos);
+        map.moveCamera(cameraUpdate);
     }
 }
